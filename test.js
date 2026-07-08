84e86c2393be7b67dfe7b95d599b9f3c08f99ad1
SATVIK SRIVASTAVA <satvikvishu1903@gmail.com>
2026-07-08 15:05:52 +0530

    WnderLust Heading added and test.js included for testing databse connectivity

---
+++ test.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.ATLASDB_URL)
+  .then(() => {
+    console.log("Connected!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
